90be0c7058e59d1ff1ad47d34cc05f4d8eb19c36
chittaranjanpanda <chittaranjan@hotmail.com>
2021-07-04 22:07:24 +0530

    Initial commit

---
+++ src/test/java/com/example/demo/order/EcomOrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo.order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomOrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
