dd8821f47703859d2185d5cf21dd4c2b4c421104
Chittaranjan Panda <19926216+pandaind@users.noreply.github.com>
2022-10-06 23:06:02 +0530

    Adding Serialization

--- src/main/java/com/example/demo/order/service/dto/OrderDTO.java
+++ src/main/java/com/example/demo/order/service/dto/OrderDTO.java
@@ -1,15 +1,14 @@
 package com.example.demo.order.service.dto;
 
 import com.example.demo.order.model.EventStatus;
-import com.example.demo.order.model.Item;
 import com.example.demo.order.model.OrderStatus;
-import com.example.demo.order.model.User;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
+import java.io.Serializable;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.List;
@@ -18,7 +17,10 @@ import java.util.List;
 @Setter
 @EqualsAndHashCode
 @ToString
-public class OrderDTO {
+public class OrderDTO implements Serializable {
+
+    private static final long serialVersionUID = -6281737652494349318L;
+
     @JsonIgnore
     private Long id;
     private String orderId;
